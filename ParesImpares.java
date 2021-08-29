dd4b0978aa43ddd96d78e8d70cbddeb723ca7c84
rangulot <rangulot@unicartagena.edu.co>
2021-08-29 17:58:15 -0500

    PAGINA DE INICIO DE SESION - En progreso

---
+++ ParesImpares.java
@@ -0,0 +1,5 @@
+public class ParesImpares {
+    public static void main(String[] args) {
+        
+    }    
+}
--- Prueba2.java
+++
@@ -1,7 +0,0 @@
-public class Prueba2 {
-    //Hello world
-        public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Hello world3");
-    }
-}
